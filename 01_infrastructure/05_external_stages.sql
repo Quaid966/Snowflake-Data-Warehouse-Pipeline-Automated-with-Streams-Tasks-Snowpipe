99dbc88d69db71e4a691270b446542cf8d79f9ac
Quaid Ali <81490514+Quaid966@users.noreply.github.com>
2025-10-02 21:18:17 +0500

    Update 05_external_stages.sql

--- 01_infrastructure/05_external_stages.sql
+++ 01_infrastructure/05_external_stages.sql
@@ -5,8 +5,9 @@
  CREATE OR REPLACE STAGE DWH.BRONZE.crm_stage
     STORAGE_INTEGRATION = s3_int
     URL = 's3://bucket/source_crm/'    --PASTE HERE YOUR source_CRM S3 URL
-    FILE_FORMAT = (TYPE = CSV FIELD_OPTIONALLY_ENCLOSED_BY='"' SKIP_HEADER=1)
-COMMENT = 'External stage for CRM data (cust_info, prd_info, sales_details) from s3://DATALAKE32/source_crm/';
+    FILE_FORMAT = (TYPE = CSV FIELD_OPTIONALLY_ENCLOSED_BY='"' SKIP_HEADER=1
+    ERROR_ON_COLUMN_COUNT_MISMATCH = FALSE)
+COMMENT = 'External stage for CRM data (cust_info, prd_info, sales_details) from s3://bucket/source_crm/';
 
 -- ========================================================
 	-- => CREATING external stages
@@ -15,5 +16,6 @@ COMMENT = 'External stage for CRM data (cust_info, prd_info, sales_details) from
  CREATE OR REPLACE STAGE DWH.BRONZE.erp_stage
     STORAGE_INTEGRATION = s3_int
     URL = 's3://bucket/source_erp/'     --PASTE HERE YOUR source_erp S3 URL
-    FILE_FORMAT = (TYPE = CSV FIELD_OPTIONALLY_ENCLOSED_BY='"' SKIP_HEADER=1)
-COMMENT = 'External stage for ERP data (cust, loc, prd_cat) from s3://DATALAKE32/source_erp/';
+    FILE_FORMAT = (TYPE = CSV FIELD_OPTIONALLY_ENCLOSED_BY='"' SKIP_HEADER=1
+    ERROR_ON_COLUMN_COUNT_MISMATCH = FALSE)
+COMMENT = 'External stage for ERP data (cust, loc, prd_cat) from s3://bucket/source_erp/';
