bde93aef9c7f914639b68d93f494c9c165c26684
Quaid Ali <81490514+Quaid966@users.noreply.github.com>
2025-10-02 12:51:34 +0500

    Update 04_schemas.sql
    add commnet

--- 01_infrastructure/04_schemas.sql
+++ 01_infrastructure/04_schemas.sql
@@ -1,29 +1,30 @@
 -- ========================================================
-	=> CREATING Bronze SCHEMA 
+	--=> CREATING Bronze SCHEMA 
 -- ========================================================
 CREATE SCHEMA IF NOT EXISTS DWH.BRONZE
 COMMENT = 'Bronze layer schema - raw ingestion of CRM and ERP data from S3 with minimal transformations';
 
 -- ========================================================
-	=> CREATING TABLE IN Silver SCHEMA
+	--=> CREATING TABLE IN Silver SCHEMA
 -- ========================================================
 CREATE SCHEMA IF NOT EXISTS DWH.SILVER
 COMMENT = 'Cleaned and standardized data model for CRM & ERP sources, transformed from Bronze layer';
 
 -- ========================================================
-	=> CREATING GOLD SCHEMA
+	--=> CREATING GOLD SCHEMA
 -- ========================================================
 CREATE SCHEMA IF NOT EXISTS DWH.GOLD
-COMMENT = 'Curated business-ready data models (fact and dimension tables) built from Silver layer for reporting and analytics.'
+COMMENT = 'Curated business-ready data models (fact and dimension tables) built from Silver layer for reporting and analytics.';
 
 -- ========================================================
-	=> CREATING TASKS Schema
+	--=> CREATING TASKS Schema
 -- ========================================================
 CREATE SCHEMA IF NOT EXISTS DWH.TASKS
 COMMENT = 'Schema to manage Snowflake tasks for orchestrating Bronze → Silver → Gold pipeline';
 
 -- ========================================================
-	=> CREATING MONITORING Schema
+	--=> CREATING MONITORING Schema
 -- ========================================================
 CREATE SCHEMA DWH.MONITORING;
+
 COMMENT = 'Centralized monitoring data quality';
