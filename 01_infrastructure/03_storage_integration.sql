9e6beac0e40d7280c2cde18d0fcf2dd953e33dd0
Quaid Ali <81490514+Quaid966@users.noreply.github.com>
2025-10-02 19:00:31 +0500

    Update 03_storage_integration.sql

--- 01_infrastructure/03_storage_integration.sql
+++ 01_infrastructure/03_storage_integration.sql
@@ -7,6 +7,6 @@ CREATE OR REPLACE STORAGE INTEGRATION s3_int
     ENABLED = TRUE
     STORAGE_AWS_ROLE_ARN = 'AWS_ROLE_ARN'
     STORAGE_ALLOWED_LOCATIONS = ('s3://bucket/')
-
 COMMENT = 'Storage integration for S3 bucket DATALAKE32, used for CRM & ERP raw data ingestion';
+
 
